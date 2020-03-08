615c7f73b92336a4175d9cbc262c723ac5021185
luischitala <itastetaco@gmail.com>
2020-03-07 21:33:06 -0600

    img route variable fixed

--- controladores/productos.controlador.php
+++ controladores/productos.controlador.php
@@ -18,6 +18,68 @@ class ControladorProductos{
 			   preg_match('/^[0-9.]+$/', $_POST["nuevoPrecioVenta"])){
             
                 $ruta = "vistas/img/productos/default/anonymous.png";
+
+                if(isset($_FILES["nuevaImagen"]["tmp_name"])){
+
+                 list($ancho, $alto) = getimagesize($_FILES["nuevaImagen"]["tmp_name"]);
+
+                 $nuevoAncho = 500;
+                 $nuevoAlto = 500;
+
+                 /*=============================================
+                 CREAMOS EL DIRECTORIO DONDE VAMOS A GUARDAR LA FOTO DEL USUARIO
+                 =============================================*/
+
+                 $directorio = "vistas/img/productos/".$_POST["nuevoCodigo"];
+
+                 mkdir($directorio, 0755);
+
+                 /*=============================================
+                 DE ACUERDO AL TIPO DE IMAGEN APLICAMOS LAS FUNCIONES POR DEFECTO DE PHP
+                 =============================================*/
+
+                 if($_FILES["nuevaImagen"]["type"] == "image/jpeg"){
+
+                     /*=============================================
+                     GUARDAMOS LA IMAGEN EN EL DIRECTORIO
+                     =============================================*/
+
+                     $aleatorio = mt_rand(100,999);
+
+                     $ruta = "vistas/img/productos/".$_POST["nuevoCodigo"]."/".$aleatorio.".jpg";
+
+                     $origen = imagecreatefromjpeg($_FILES["nuevaImagen"]["tmp_name"]);						
+
+                     $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+                     imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+                     imagejpeg($destino, $ruta);
+
+                 }
+
+                 if($_FILES["nuevaImagen"]["type"] == "image/png"){
+
+                     /*=============================================
+                     GUARDAMOS LA IMAGEN EN EL DIRECTORIO
+                     =============================================*/
+
+                     $aleatorio = mt_rand(100,999);
+
+                     $ruta = "vistas/img/productos/".$_POST["nuevoCodigo"]."/".$aleatorio.".png";
+
+                     $origen = imagecreatefrompng($_FILES["nuevaImagen"]["tmp_name"]);						
+
+                     $destino = imagecreatetruecolor($nuevoAncho, $nuevoAlto);
+
+                     imagecopyresized($destino, $origen, 0, 0, 0, 0, $nuevoAncho, $nuevoAlto, $ancho, $alto);
+
+                     imagepng($destino, $ruta);
+
+                 }
+
+             }
+                
                 $tabla = "productos";
 
 				$datos = array("id_categoria" => $_POST["nuevaCategoria"],
--- vistas/js/productos.js
+++ vistas/js/productos.js
@@ -134,4 +134,53 @@ $(".porcentaje").on("ifchecked", function(){
     $("#nuevoPrecioVenta").prop("readonly", true);
 })
    
+/*=============================================
+SUBIENDO LA FOTO DEL Producto
+=============================================*/
+$(".nuevaImagen").change(function(){
+
+	var imagen = this.files[0];
+	
+	/*=============================================
+  	VALIDAMOS EL FORMATO DE LA IMAGEN SEA JPG O PNG
+  	=============================================*/
+
+  	if(imagen["type"] != "image/jpeg" && imagen["type"] != "image/png"){
+
+  		$(".nuevaImagen").val("");
+
+  		 swal({
+		      title: "Error al subir la imagen",
+		      text: "¡La imagen debe estar en formato JPG o PNG!",
+		      type: "error",
+		      confirmButtonText: "¡Cerrar!"
+		    });
+
+  	}else if(imagen["size"] > 2000000){
+
+  		$(".nuevaImagen").val("");
+
+  		 swal({
+		      title: "Error al subir la imagen",
+		      text: "¡La imagen no debe pesar más de 2MB!",
+		      type: "error",
+		      confirmButtonText: "¡Cerrar!"
+		    });
+
+  	}else{
+
+  		var datosImagen = new FileReader;
+  		datosImagen.readAsDataURL(imagen);
+
+  		$(datosImagen).on("load", function(event){
+
+  			var rutaImagen = event.target.result;
+
+  			$(".previsualizar").attr("src", rutaImagen);
+
+  		})
+
+  	}
+})
+
 
